e4f02843dedd66d54780090b43300b86e8e6da4e
Anna Koroleva <aakorolyova@gmail.com>
2023-12-19 18:31:18 +0000

    Static Q&A

---
+++ src/static_qna_langchain.py
@@ -0,0 +1,58 @@
+from pathlib import Path
+
+from langchain.agents.agent_toolkits import (
+    create_conversational_retrieval_agent,
+    create_retriever_tool,
+)
+from langchain.chains.question_answering import load_qa_chain
+from langchain.chat_models import ChatOpenAI
+from langchain.document_loaders import PyPDFLoader
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import FAISS
+
+llm = ChatOpenAI(temperature=0.7)
+path = path = Path("data/StaticDocs")
+docs = []
+for f in path.absolute().iterdir():
+    docs.extend(PyPDFLoader(str(f)).load())
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=10)
+chunked_documents = text_splitter.split_documents(docs)
+
+embeddings = OpenAIEmbeddings()
+db = FAISS.from_documents(chunked_documents, embeddings)
+
+llm = ChatOpenAI(model="gpt-3.5-turbo")
+chain = load_qa_chain(llm, chain_type="stuff")
+
+
+retriever = db.as_retriever()
+tool = create_retriever_tool(
+    retriever,
+    "search_disaster_relief",
+    "Searches and returns documents regarding disaster relief.",
+)
+tools = [tool]
+agent_executor = create_conversational_retrieval_agent(llm, tools, verbose=True)  # type: ignore
+
+
+# %%
+questions = [
+    "What emergency numbers do I call in the USA?",
+    "I need emergency shelter, how do I find one?",
+    "Somebody is bleeding here, how can I help them?",
+    "How do I help bleeding?",
+    "How do I help someone with a broken leg?",
+    "My area is prone to flooding, how can I prepare?",
+    "How do I escape from water in case of flood?",
+    "I am caught up in flood and no help is available, how can I rescue myself?",
+    "I am caught up in flood and no help is available, how do I get to safety?",
+    "I am caught up in flood and no help is available, what do I do?",
+]
+
+
+for question in questions:
+    response = agent_executor({"input": question})
+    print(question)
+    print(response)
+    print()
---
+++ src/static_qna_llamaindex.py
@@ -0,0 +1,54 @@
+from __future__ import absolute_import
+
+from pathlib import Path
+
+from llama_index import SimpleDirectoryReader, VectorStoreIndex
+from llama_index.prompts import PromptTemplate
+
+# %%
+model = "gpt-3.5-turbo-1106"
+print(Path("data/StaticDocs").absolute())
+docs = SimpleDirectoryReader(
+    input_dir=str(Path("data/StaticDocs").absolute())
+).load_data()
+
+# %%
+index = VectorStoreIndex.from_documents(docs)
+query_engine = index.as_query_engine(response_mode="compact")
+
+prompts_dict = query_engine.get_prompts()
+print(prompts_dict)
+
+new_summary_tmpl_str = (
+    "Context information is below."
+    "---------------------"
+    "{context_str}"
+    "---------------------"
+    "Given the context information and not prior knowledge, answer the query."
+    "The user is a person in an area hit by a disaster, potentially in distress."
+    "You answers must be brief, precise and as actionable as can be based on the context information."
+    "Query: {query_str}"
+    "Answer: "
+)
+new_summary_tmpl = PromptTemplate(new_summary_tmpl_str)
+query_engine.update_prompts({"response_synthesizer:summary_template": new_summary_tmpl})
+# %%
+questions = [
+    "What emergency numbers do I call in the USA?",
+    "I need emergency shelter, how do I find one?",
+    "Somebody is bleeding here, how can I help them?",
+    "How do I help bleeding?",
+    "How do I help someone with a broken leg?",
+    "My area is prone to flooding, how can I prepare?",
+    "How do I escape from water in case of flood?",
+    "I am caught up in flood and no help is available, how can I rescue myself?",
+    "I am caught up in flood and no help is available, how do I get to safety?",
+    "I am caught up in flood and no help is available, what do I do?",
+]
+
+
+for question in questions:
+    response = query_engine.query(question)
+    print(question)
+    print(response)
+    print()
